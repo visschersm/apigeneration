2db8f8efb576f0e9fcc725d868d4bcf30ab84242
Mark Visschers <visschers.m@gmail.com>
2024-11-17 18:13:31 +0100

    implements crud in minimal api

--- Hogwarts.Api.NetMinimalApi/Program.cs
+++ Hogwarts.Api.NetMinimalApi/Program.cs
@@ -1,3 +1,7 @@
+using Hogwarts.Api.Service;
+using Hogwarts.Api.Domain;
+using Hogwarts.Api.NetContracts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,12 +27,58 @@ var students = new Student[]
 
 var studentsGroup = app.MapGroup("/students");
 
+studentsGroup.MapPost("/", async (Hogwarts.Api.NetContracts.Student student, StudentService service) =>
+{
+    var domainStudent = new Hogwarts.Api.Domain.Student
+    {
+        FirstName = student.FirstName,
+        Surname = student.Surname
+    };
+    await service.Create(domainStudent);
+});
+
 studentsGroup.MapGet("/", async (StudentService service) =>
-    await service.GetStudents());
+{
+    var students = await service.Get();
+    return students.Select(student => new Hogwarts.Api.NetContracts.Student
+    {
+        Id = student.Id,
+        FirstName = student.FirstName,
+        Surname = student.Surname
+    });
+});
+
 
 studentsGroup.MapGet("/{id}", async (int id, StudentService service) =>
-    await service.GetStuent(id);
+{
+    var student = await service.Get(id);
 
+    if (student is null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(new Hogwarts.Api.NetContracts.Student
+    {
+        FirstName = student.FirstName,
+        Surname = student.Surname,
+    });
+});
+
+studentsGroup.MapPut("/{id}", async (int id, Hogwarts.Api.NetContracts.Student inputStudent, StudentService service) =>
+{
+    var student = new Hogwarts.Api.Domain.Student
+    {
+        FirstName = inputStudent.FirstName,
+        Surname = inputStudent.Surname,
+    };
+    await service.Update(inputStudent.Id, student);
+});
+
+studentsGroup.MapDelete("/{id}", async (int id, StudentService service) =>
+{
+    await service.Delete(id);
+});
 
 app.Run();
 
